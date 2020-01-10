bd9ca262b06006c322bd7bc4d5d9b718e46d14cf
zdc <zdc>
2020-01-10 14:23:38 +0800

    提交controller

---
+++ src/main/java/com/qdtz/SpringWebService/controller/HelloController.java
@@ -0,0 +1,36 @@
+package com.qdtz.SpringWebService.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.qdtz.SpringWebService.service.TestOracleService;
+
+@Controller
+public class HelloController {
+    
+    @Autowired
+    private TestOracleService testOracleService;
+    
+    @ResponseBody
+    @GetMapping("/hello")
+    public String hello(){
+        return "hello spring boot";
+    }
+    
+    @ResponseBody
+    @GetMapping(value="/userCount")
+    public Integer getUserCount() {
+        
+        return testOracleService.getSlaveUserCount();
+    }
+    
+    @ResponseBody
+    @GetMapping(value="/slaveUserCount")
+    public Integer getSlaveUserCount() {
+        
+        return testOracleService.getUserCount();
+    }
+
+}
