d1395615a6d6ce6256bbe0940ac955fd337717fa
zdc <zdc>
2020-01-10 14:24:31 +0800

    提交多数据源配置及mapper

---
+++ src/main/java/com/qdtz/SpringWebService/config/MasterDataSourceConfiguration.java
@@ -0,0 +1,51 @@
+package com.qdtz.SpringWebService.config;
+
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.SqlSessionTemplate;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+
+import com.alibaba.druid.pool.DruidDataSource;
+
+import javax.sql.DataSource;
+ 
+@Configuration
+@MapperScan(basePackages = "com.qdtz.SpringWebService.mapper.master", sqlSessionTemplateRef  = "masterSqlSessionTemplate")
+public class MasterDataSourceConfiguration {
+ 
+    @Bean(name = "masterDataSource")
+    @Primary//配置默认数据源
+    @ConfigurationProperties(prefix = "spring.datasource.druid.masterDB")
+    public DataSource dataSource() {
+        return  new DruidDataSource();
+    }
+ 
+    @Bean(name = "masterSqlSessionFactory")
+    public SqlSessionFactory sqlSessionFactory(@Qualifier("masterDataSource") DataSource dataSource) throws Exception {
+        SqlSessionFactoryBean bean = new SqlSessionFactoryBean();
+        bean.setDataSource(dataSource);
+        bean.setMapperLocations(new PathMatchingResourcePatternResolver().getResources("com/qdtz/SpringWebService/mapper/Impl/*.xml"));
+        return bean.getObject();
+    }
+ 
+    @Bean(name = "masterTransactionManager")
+    public DataSourceTransactionManager transactionManager(@Qualifier("masterDataSource") DataSource dataSource) {
+        return new DataSourceTransactionManager(dataSource);
+    }
+ 
+    @Bean(name = "masterSqlSessionTemplate")
+    public SqlSessionTemplate sqlSessionTemplate(@Qualifier("masterSqlSessionFactory") SqlSessionFactory sqlSessionFactory)
+            throws Exception {
+        return new SqlSessionTemplate(sqlSessionFactory);
+    }
+ 
+    
+ 
+}
---
+++ src/main/java/com/qdtz/SpringWebService/mapper/slave/TestSlaveMapper.java
@@ -0,0 +1,8 @@
+package com.qdtz.SpringWebService.mapper.slave;
+
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface TestSlaveMapper {
+    Integer getSlaveUserCount();
+}
