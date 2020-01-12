8ec3434fc24d8a3bbd3e1a1850cac3135278c12f
zhangdc <49420442+zdc631541485@users.noreply.github.com>
2020-01-12 16:54:08 +0800

    测试

--- src/main/java/com/qdtz/SpringWebService/controller/HelloController.java
+++ src/main/java/com/qdtz/SpringWebService/controller/HelloController.java
@@ -16,7 +16,7 @@ public class HelloController {
     @ResponseBody
     @GetMapping("/hello")
     public String hello(){
-        return "hello spring boot";
+        return "hello spring boot1";
     }
     
     @ResponseBody
