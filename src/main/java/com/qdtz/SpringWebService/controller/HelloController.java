6f3956e41a47b7f3a6660cc466c0875cfc2f9c6b
zhangdc <49420442+zdc631541485@users.noreply.github.com>
2020-02-04 16:26:22 +0800

    在静态页面取值

--- src/main/java/com/qdtz/SpringWebService/controller/HelloController.java
+++ src/main/java/com/qdtz/SpringWebService/controller/HelloController.java
@@ -2,6 +2,7 @@ package com.qdtz.SpringWebService.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -20,7 +21,8 @@ public class HelloController {
     }
     
     @GetMapping("/toTestHtml")
-    public String testHtml(){
+    public String testHtml(Model model){
+    	model.addAttribute("msg", "测试模块的取值！！！");
         return "test";
     }
     
