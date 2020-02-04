6d878d3afea54f3c92b557f5f96ed2d40591c4cd
zhangdc <49420442+zdc631541485@users.noreply.github.com>
2020-02-04 22:30:43 +0800

    优化代码格式

--- src/main/java/com/qdtz/SpringWebService/service/Impl/TestOracleServiceImpl.java
+++ src/main/java/com/qdtz/SpringWebService/service/Impl/TestOracleServiceImpl.java
@@ -10,16 +10,13 @@ import com.qdtz.SpringWebService.mapper.slave.TestSlaveMapper;
 import com.qdtz.SpringWebService.service.TestOracleService;
 
 /**
- * 
  * @version: v1.0.0
  * @Description: serviceName 对外发布的服务名,targetNamespace 指定你想要的名称空间，通常使用使用包名反转,
- *               endpointInterface 服务接口全路径, 指定做SEI（Service EndPoint
- *               Interface）服务端点接口
+ *               endpointInterface 服务接口全路径, 指定做SEI（Service EndPoint Interface）服务端点接口
  * @author: ZDC
  * @date: 2020年1月9日 上午11:27:17
  */
-@WebService(serviceName = "UserService",
-        targetNamespace = "http://service.demo.example.com")
+@WebService(serviceName = "UserService", targetNamespace = "http://service.demo.example.com")
 @Component
 public class TestOracleServiceImpl implements TestOracleService {
     
