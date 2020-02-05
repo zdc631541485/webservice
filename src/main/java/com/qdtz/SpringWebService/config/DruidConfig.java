e9396a4228737cbd3fd318fba8e9d2a1a0ab5b27
zhangdc <49420442+zdc631541485@users.noreply.github.com>
2020-02-05 11:51:37 +0800

    删除无用的导入

--- src/main/java/com/qdtz/SpringWebService/config/DruidConfig.java
+++ src/main/java/com/qdtz/SpringWebService/config/DruidConfig.java
@@ -1,15 +1,11 @@
 package com.qdtz.SpringWebService.config;
 
-import com.alibaba.druid.pool.DruidDataSource;
 import com.alibaba.druid.support.http.StatViewServlet;
 import com.alibaba.druid.support.http.WebStatFilter;
-import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import javax.sql.DataSource;
 
 /**
  * druid监控控制台配置 
